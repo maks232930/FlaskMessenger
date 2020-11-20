00926173eef1eadf27be74fbbf82698c05916f17
Maksim Pukshlis <maksimpusklis7@gmail.com>
2020-11-20 03:18:57 +0300

    create BaseModel

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask
-# from flask_migrate import Migrate
 from config.config import config
 
 
 def create_app(app_config='development'):
     app = Flask(__name__)
     app.config.from_object(config[app_config])
-    from app.main.routers import main
 
-    app.register_blueprint(main)
     return app
-
-# migrate = Migrate(app, db)
--- app/model/models.py
+++ app/model/models.py
@@ -1,1 +1,17 @@
-from app.database import db
+from sqlalchemy import DateTime
+
+from app.database.db import db
+from sqlalchemy.sql import func
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    created_at = db.Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+        return "<{0.__class__.__name__}(id={0.id!r})>".format(self)
+
+
