d9c74fb64654d5ef3830afa4d2535f2ceb228f62
Maksim Pukshlis <maksimpusklis7@gmail.com>
2020-12-08 13:29:10 +0300

    created chat_api

--- app/api/chat_api.py
+++ app/api/chat_api.py
@@ -5,7 +5,7 @@ from sqlalchemy import or_
 
 from app.extensions import db
 from app.model.chat import Chat, chats_schema
-from app.model.message import Message
+from app.model.message import Message, messages_schema
 
 
 class ChatsApi(Resource):
@@ -31,20 +31,24 @@ class ChatApi(Resource):
     @jwt_required
     def get(self, chat_id):
         user_id = get_jwt_identity()
-        messages = Message.query(Message.chat_id == chat_id)
-        messages.filter(Message.chat.second_participant_id == user_id)
-        # ses.query(FooBar).join(Foobar.bar).join(Bar.foo).filter(Foo.name == "blah")
+        chat = Chat.query.filter(Chat.id == chat_id,
+                                 or_(Chat.first_participant_id == user_id,
+                                     Chat.second_participant_id == chat_id)).filter(Message.is_delete != False)
+        if chat is None:
+            return {"Response": "chat_id invalid"}, 404
+        messages = Message.query.join(Chat).filter(Message.chat_id == Chat.id).filter(Message.chat_id == chat_id)
+        return jsonify(messages_schema.dump(messages))
 
-        # if messages is None:
-        #     return {'response': "Not Found"}, 404
-        # return jsonify(messages_schema.dump(messages))
-        return "None"
-
-    # @jwt_required
-    # def post(self, chat_id):
-    #     user_id = get_jwt_identity()
-    #     chat = Chat.query.filter(Chat.id == chat_id,
-    #                              or_(Chat.first_participant_id == user_id, Chat.second_participant_id == user_id))
-    #     body = request.get_json()
-    #     Message(**body, sender=user_id, chat_id=chat_id)
-    #     return {"response": "Ok"}, 200
+    @jwt_required
+    def post(self, chat_id):
+        user_id = get_jwt_identity()
+        chat = Chat.query.filter(Chat.id == chat_id,
+                                 or_(Chat.first_participant_id == user_id,
+                                     Chat.second_participant_id == user_id)).first()
+        if chat is None:
+            return {"response": "chat_id invalid"}, 404
+        body = request.get_json()
+        message = Message(**body, sender_id=user_id, chat=chat)
+        db.session.add(message)
+        db.session.commit()
+        return {"response": "Ok"}, 201
--- app/model/chat.py
+++ app/model/chat.py
@@ -8,14 +8,14 @@ from .user import User
 
 class Chat(Base):
     __tablename__ = 'chats'
-    name = Column(VARCHAR, nullable=False)
+    name = Column(VARCHAR(50), nullable=False)
     first_participant_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
-    second_participant_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False, unique=True)
+    second_participant_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
 
     first_participant = relationship(User, post_update=True, foreign_keys=[first_participant_id],
                                      backref='first_user')
     second_participant = relationship(User, post_update=True, foreign_keys=[second_participant_id],
-                                      backref='second_user')
+                                      backref='second_user', uselist=False)
 
 
 class ChatSchema(ma.SQLAlchemyAutoSchema):
